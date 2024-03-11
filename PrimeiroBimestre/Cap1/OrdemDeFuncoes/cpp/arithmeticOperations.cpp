41fd9723ae5d7eab3912df9b9f60ceecb937e4e1
Ramon Fernandes Viana <ramon.fv95@gmail.com>
2024-03-10 22:18:40 -0300

    arithmetics operations

---
+++ PrimeiroBimestre/Cap1/OrdemDeFuncoes/cpp/arithmeticOperations.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main(){
+
+
+
+    int students = 20;
+
+    double students = 20;
+
+    // students+=1;
+    // students++;
+
+    // students = students * 2;
+
+    // students/=2;
+
+    int remainder = students % 2;
+
+    std::cout << remainder;
+
+    return 0;
+
+
+}
